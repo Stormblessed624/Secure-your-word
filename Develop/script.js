feb82c527d334eeaa7bd9b10c97fc7c50db2a130
Matt <matt.white.624@gmail.com>
2021-11-07 22:35:55 -0700

    added initial word length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,31 @@
 // Assignment code here
+function generatePassword() {
+  // Define character options
+  var lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+  var upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var specialChars = '!@#$%^&*()';
+  var numbers = '0123456789';
 
+  var confirmUppercase;
+  var confirmLowercase;
+  var confirmSpecial;
+  var confirmNumbers;
+
+  // Prompt user for character limit between 8 and 128
+  wordLength = parseInt(window.prompt("How long would you like your password? min 8, max 128"));
+  if(!wordLength) {
+    window.alert("You need to give a password length!")
+  } 
+  else if (wordLength < 8 || wordLength > 128) {
+    window.alert("Please select a valid length.")
+
+    wordLength = parseInt(window.prompt("Your password must be between 8 and 128 characters. How long would you like it to be?"));
+  } else {
+    confirmUppercase = confirm("will this contain numbers?")
+  }
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
